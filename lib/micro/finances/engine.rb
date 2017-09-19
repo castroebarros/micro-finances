8dbfc3057490df1da05e9fab849fec4be9a3b690
Lucas Castro <castro.lucas@gmail.com>
2017-09-19 15:05:45 -0300

    Loading decorators to provide more extensibility

--- lib/micro/finances/engine.rb
+++ lib/micro/finances/engine.rb
@@ -2,6 +2,12 @@ module Micro
   module Finances
     class Engine < ::Rails::Engine
       isolate_namespace Micro::Finances
+
+      config.to_prepare do
+        Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+          require_dependency(c)
+        end
+      end
     end
   end
 end
